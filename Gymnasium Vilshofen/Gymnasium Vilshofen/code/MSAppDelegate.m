5b38b02fe056772d826e524f0fcd01a9ff2b23aa
Michael Mayerhofer <michael@4mayerhofers.de>
2014-02-26 20:01:41 +0100

    Push endlich funktionstüchtig

--- Gymnasium Vilshofen/Gymnasium Vilshofen/code/MSAppDelegate.m
+++ Gymnasium Vilshofen/Gymnasium Vilshofen/code/MSAppDelegate.m
@@ -10,20 +10,6 @@
 
 @implementation MSAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:
-                                                                UIApplicationBackgroundFetchIntervalMinimum];
-    
-    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
-    
-    //Farbe der Navigation-Bar auf rot ändern
-    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:(205.0/255) green:(42.0/255) blue:(42.0/255) alpha:1]];
-    
-    
-    return YES;
-}
-
 -(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
 {
     
@@ -81,7 +67,7 @@
 {
     [[LocalyticsSession shared] close];
     [[LocalyticsSession shared] upload];
-
+    
 }
 
 -(void)application:(UIApplication *)application
@@ -115,13 +101,13 @@ performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionH
         NSLog(@"Klasse: %@", klasseNeu);
         
         NSArray *klasseAlt = [[[NSUserDefaults standardUserDefaults] dictionaryForKey:@"vPlan"]
-                                objectForKey: [[NSUserDefaults standardUserDefaults] objectForKey:@"klasse"]];
+                              objectForKey: [[NSUserDefaults standardUserDefaults] objectForKey:@"klasse"]];
         
         if (![klasseNeu isEqualToArray: klasseAlt] && klasseAlt != nil) {
             
             
             NSString *message = @"Es gibt Änderungen am Vertretungsplan";
-
+            
             
             [self sendNotificationWithMessage: message];
             
@@ -144,7 +130,7 @@ performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionH
         
         [[NSUserDefaults standardUserDefaults] setObject:json forKey:@"vPlan"];
     }
-
+    
     completionHandler(UIBackgroundFetchResultNewData);
 }
 
@@ -153,7 +139,7 @@ performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionH
     NSLog(@"Send notification 1");
     if([[NSUserDefaults standardUserDefaults] boolForKey:@"background"]) {
         NSLog(@"Send notification 2");
-    
+        
         UILocalNotification *localNotif = [[UILocalNotification alloc] init];
         localNotif.alertBody = message;
         localNotif.alertAction = @"Öffnen";
@@ -166,54 +152,133 @@ performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionH
 
 #pragma mark RemoteNotification
 
-// Wird aufgerufen wenn Nachricht ankommt und die App aktiv ist
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"Device Token=%@", deviceToken);
+    
+    if ([[NSUserDefaults standardUserDefaults] valueForKey:@"name"]){
+        
+        NSString* Token = [NSString stringWithFormat:@"%@", deviceToken];
+        Token = [Token substringWithRange:NSMakeRange(1, Token.length - 2)];
+        Token = [Token stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        
+        NSString *name = [[NSUserDefaults standardUserDefaults] valueForKey:@"name"];
+        NSLog(@"%@", name);
+        NSString* URLname = [name stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSLog(@"%@", URLname);
+        NSString* url = [NSString stringWithFormat:@"http://www.4mayerhofers.de/Push/addDeviceToken.php?deviceToken=%@&name=%@", Token, URLname];
+        
+        NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+        
+        [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *urlResponse, NSData *data, NSError *error) {
+            if (error) {
+                NSLog(@"Error beim senden des Tokens");
+            }
+            else{
+                NSLog(@"Token wurde gesendt");
+            }
+        }];
+    }
+    else{
+        UIAlertView *alertViewname = [[UIAlertView alloc] initWithTitle:@"Gib hier bitte deinen Namen ein" message:@"Name eingeben" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        alertViewname.tag = 2;
+        alertViewname.alertViewStyle = UIAlertViewStylePlainTextInput;
+        [alertViewname show];
+    }
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+	
+	NSLog(@"Error: %@", [error localizedDescription]);
+}
+
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
     
-    [UIApplication sharedApplication].applicationIconBadgeNumber = [[[userInfo objectForKey:@"aps"] objectForKey: @"badge"] intValue];
-    NSString* alert = [[userInfo objectForKey:@"aps"] objectForKey: @"alert"];
-    NSString* alerttitle = [[userInfo objectForKey:@"aps"] objectForKey: @"alerttitle"];
-    NSString* code = [[userInfo objectForKey:@"aps"] objectForKey: @"code"];
+    [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
     
-    if ([code isEqual: @"testcode"]){
-        //stuff
-    }
+    NSLog(@"application didReceiveRemoteNotification");
     
-    NSLog(@"remote notification: %@",[userInfo description]);
-    NSLog(@"Nachricht: %@", alert);
+    NSString* alerttype = [userInfo objectForKey:@"alerttype"];
+    NSString* alert =  [[userInfo objectForKey:@"aps" ] objectForKey:@"alert"];
+    NSString* alerttext = [userInfo objectForKey:@"alerttext"];
+    NSString* Cancel = [userInfo objectForKey:@"cancel"];
+    NSString* Button1 = [userInfo objectForKey:@"button1"];
+    
+    NSLog(@"%@", userInfo);
+    
+    NSLog(@"Type: %@", alerttype);
+    NSLog(@"Title: %@", alert);
+    NSLog(@"Nachricht: %@", alerttext);
+    NSLog(@"Cancel: %@", Cancel);
+    NSLog(@"Button1: %@", Button1);
     
     UIApplicationState state = [[UIApplication sharedApplication] applicationState];
     if (state == UIApplicationStateActive) {
-        UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:alerttitle message:alert delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
-        [alertView show];
+        
+        if ([alerttype isEqualToString:@"1cb"]){
+            UIAlertView* alertView1cb = [[UIAlertView alloc] initWithTitle:alert message:alerttext delegate:nil cancelButtonTitle:Cancel otherButtonTitles:nil];
+            [alertView1cb show];
+        }
+        if ([alerttype isEqualToString:@"name"]){
+            UIAlertView *alertViewname = [[UIAlertView alloc] initWithTitle:alert message:alerttext delegate:self cancelButtonTitle:Cancel otherButtonTitles:nil] ;
+            alertViewname.tag = 2;
+            alertViewname.alertViewStyle = UIAlertViewStylePlainTextInput;
+            [alertViewname show];
+        }
+    }
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:
+     UIApplicationBackgroundFetchIntervalMinimum];
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+    
+    //Farbe der Navigation-Bar auf rot ändern
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:(205.0/255) green:(42.0/255) blue:(42.0/255) alpha:1]];
+    
+    
+    return YES;
+    
+    if ([launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey]) {  //Achtung!! diese Methode wird nur aufgerufen wenn die App nicht läuft, und man gleich nach dem die Notification eingetroffen ist auf dem Springboard Start drückt
+        
+        NSString* alerttype = [[launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey] objectForKey:@"alerttype"];
+        NSString* alerttext = [[launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey] objectForKey:@"alerttext"];
+        NSString* alert = [[[launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey] objectForKey:@"aps"] objectForKey:@"alert"];
+        NSString* Cancel = [[launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey] objectForKey:@"cancel"];
+        NSString* Button1 = [[launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey] objectForKey:@"button1"];
+        
+        NSLog(@"Type: %@", alerttype);
+        NSLog(@"Title: %@", alerttext);
+        NSLog(@"Nachricht: %@", alert);
+        NSLog(@"Cancel: %@", Cancel);
+        NSLog(@"Button1: %@", Button1);
+        
+        if ([alerttype isEqualToString:@"1cb"]){
+            UIAlertView* alertView1cb = [[UIAlertView alloc] initWithTitle:alert message:alerttext delegate:nil cancelButtonTitle:Cancel otherButtonTitles:nil];
+            [alertView1cb show];
+        }
+        if ([alerttype isEqualToString:@"name"]){
+            UIAlertView *alertViewname = [[UIAlertView alloc] initWithTitle:alert message:alerttext delegate:self cancelButtonTitle:Cancel otherButtonTitles:nil] ;
+            alertViewname.tag = 2;
+            alertViewname.alertViewStyle = UIAlertViewStylePlainTextInput;
+            [alertViewname show];
+        }
     }
     
+	[[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge |
+                                                                           UIRemoteNotificationTypeSound)];
+	
+    return YES;
 }
 
-// Hier wird der Device Token audgegeben unter dem das Gerät errreichbar ist
--(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+- (void)alertView:(UIAlertView *)alertViewname clickedButtonAtIndex:(NSInteger)buttonIndex {
     
-    NSLog(@"Device Token=%@", deviceToken);
+    NSString* name =[alertViewname textFieldAtIndex:0].text;
     
-    NSString* token = [NSString stringWithFormat:@"%@", deviceToken];
+    [[NSUserDefaults standardUserDefaults] setValue: name forKey: @"name"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
     
-    token= [token substringWithRange:NSMakeRange(1, token.length - 2)];
-    
-    token = [token stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    
-    NSString* url = [NSString stringWithFormat:@"http://michaelmayer.cwsurf.de/MYSQL/addDeviceToken.php?deviceToken=%@", token];
-    
-    NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
-    
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *urlResponse, NSData *data, NSError *error) {
-        if (error) {
-            NSLog(@"Error");
-        }
-    }];
-}
-
-// Wird aufgerufen wenn es einen Error gab
--(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
-    NSLog(@"Fehler bei der Registrierung: %@", error);
 }
 
 
